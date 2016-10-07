7d556626d3b4869ec689973ad3a4766f03c0698a
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-07 15:50:12 +0200

    Fixed static variables and removed apache comments

--- javalib/src/main/scala/java/io/File.scala
+++ javalib/src/main/scala/java/io/File.scala
@@ -1,15 +1,5 @@
 package java.io
 
-
-/*
-	note: All the @static are here to denote
-	the fact that all those field were static in their 
-	java iteration, I will find a way to express them 
-	correctly later.
-
-	same thing for @transient
- */
-
 class File private () extends Serializable with Comparable[File] {
   	def this(path: String) = this()
   	def this(parent: String, child: String) = this()
@@ -17,63 +7,25 @@ class File private () extends Serializable with Comparable[File] {
 
   	def compareTo(file: File): scala.Int = ???
 
-    @static private val serialVersionUID: Long = 301077366599181567L;
+  	object Statîcs{
+		val separatorChar: Char;
+		val separator: String;
+	    val pathSeparatorChar: Char;
+	    val pathSeparator: String;
+	    private var counter: Int = 0;
+	    private var counterBase: Int = 0;
+	    private class TempFileLocker{}
+	    private val tempFileLocker: TempFileLocker = new TempFileLocker()
+	    private var caseSensitive: Boolean;
+  	}
 
-    @static private val EMPTY_STRING: String = ""; //$NON-NLS-1$
 
   	private var path: String;
 
-  	@transient var properPath: Array[Byte]; 
+  	/*TODO: find how to make transient work in Scala.*/
+  	//@transient var properPath: Array[Byte]; 
 
-	/**
-	 * The system dependent file separator character.
-	 */
-	@static val separatorChar: Char;
 
-	/**
-	 * The system dependent file separator string. The initial value of this
-	 * field is the system property "file.separator".
-	 */
-	@static val separator: String;
-
-    /**
-     * The system dependent path separator character.
-     */
-    @static val pathSeparatorChar: Char;
-
-    /**
-     * The system dependent path separator string. The initial value of this
-     * field is the system property "path.separator".
-     */
-    @static val pathSeparator: String;
-
-    /* Temp file counter TODO : should be static*/
-    @static private var counter: Int = 0;
-
-    /* identify for different VM processes */
-    @static private var counterBase: Int = 0;
-
-    @static private class TempFileLocker{}
-
-    @static private TempFileLocker tempFileLocker = new TempFileLocker();
-
-    @static private var caseSensitive: Boolean;
-
-    @static private def oneTimeInitialization(): Unit = {
-
-    }
-
-    //are constructor sensed to be overrided this way in Scala ?
-	/**
-	 * Constructs a new file using the specified directory and name.
-	 * 
-	 * @param dir
-	 *            the directory where the file is stored.
-	 * @param name
-	 *            the file's name.
-	 * @throws NullPointerException
-	 *             if {@code name} is {@code null}.
-	 */
 	def this(File dirPath, String name): File = {
 		if(name == null) throw NullPointerException()
 		if(dir == null) path = fixSashes(name)
@@ -81,35 +33,12 @@ class File private () extends Serializable with Comparable[File] {
 		this()
 	}
 
-
-    /**
-     * Constructs a new file using the specified path.
-     * 
-     * @param path
-     *            the path to be used for the file.
-     */
     def this(path: String): File {
-        // path == null check & NullPointerException thrown by fixSlashes
         this.path = fixSlashes(path)
         this()
     }
 
-    /**
-     * Constructs a new File using the path of the specified URI. {@code uri}
-     * needs to be an absolute and hierarchical Unified Resource Identifier with
-     * file scheme and non-empty path component, but with undefined authority,
-     * query or fragment components.
-     * 
-     * @param uri
-     *            the Unified Resource Identifier that is used to construct this
-     *            file.
-     * @throws IllegalArgumentException
-     *             if {@code uri} does not comply with the conditions above.
-     * @see #toURI
-     * @see java.net.URI
-     */
     def File(URI uri): File {
-        // check pre-conditions
         checkURI(uri);
         this.path = fixSlashes(uri.getPath());
         this()
@@ -118,21 +47,20 @@ class File private () extends Serializable with Comparable[File] {
     private def calculePath(dirPath: String, name: Sting): String = {
     	val path: String = fixSlashes(dirPath)
     	if(!name.isEmpty || path.isEmpty){
-    		//Remove all the proceeding separator chars from name
     		var name: String = fixSlashes(name)
     		var separatorIndex: Int = 0;
 
     		while(separatorIndex < name.length() && 
-    			name(separatorIndex) == separatorChar){
+    			name(separatorIndex) == Statics.separatorChar){
     			speratorIndex++
     		}
 
     		if(separatorIndex > 0){
-    			name = name.substring(separatorIndex, name.length())
+    			name = name.substring(separatorIndex, name.length(	))
     		}
-    		// Ensure there is a separator char between dirPath and name
+
     		val pathLength: Int = path.length()
-    		if(pathLength > 0 && path(pathLength-1 == separatorChar){
+    		if(pathLength > 0 && path(pathLength-1 == Statics.separatorChar){
     			path + name;
     		} else path + separatorChar + name
     	}
