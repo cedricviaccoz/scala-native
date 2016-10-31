dea48694e5040a7c2eb573e7c4383fee0cc15fa0
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-31 18:32:14 +0100

    Creation of time library

---
+++ nativelib/src/main/scala/scala/scalanative/posix/time.scala
@@ -0,0 +1,19 @@
+package scala.scalanative
+package posix
+
+import scala.scalanative.native.{struct, extern, Ptr}
+
+@extern
+object time {
+
+	type time_t = Long
+	type suseconds_t = Long
+
+	@struct
+	class timeval(
+		val tv_sec: time_t,
+		val tv_usec: time_t
+	)
+
+	def gettimeofday(tv: Ptr[timeval], tz: Ptr[_]): Unit = extern
+}
