57a13e0f793c5f0026bf08334f5d45e20d2471bc
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-31 18:34:03 +0100

    altered tests to check against the past time et a very long future time

--- scripted-tests/run/java-lang-System-current-time/src/main/scala/currentTimeTest.scala
+++ scripted-tests/run/java-lang-System-current-time/src/main/scala/currentTimeTest.scala
@@ -1,10 +1,19 @@
 import scala.scalanative.native._, stdio._
 import java.lang.System
 
-object Hello {
+object currentTimeTest {
   def main(args: Array[String]): Unit = {
-  	val d: Long = System.getCurrentMillis()
-    fprintf(stderr, c"Hello, world!")
-    assert(d != 0)
+  	val time: Long = System.currentTimeMillis()
+  	
+  	//current system time on a 64-bit unix system representing 
+  	// 31 october 2016, 16:18:04
+  	val halloweenTime: Long = 1477927084326L
+
+  	//maximum value possible for a 64-bit signed integer
+  	val theLongestTime: Long = 9223372036854775807L
+
+  	assert(time > 0)
+    assert(time > halloweenTime)
+    assert(time < theLongestTime)
   }
 }
